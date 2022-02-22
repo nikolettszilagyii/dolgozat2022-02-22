0054eec49af5a101a049fc9796cd42cb8b7a34d9
Szilágyi Nikolett <nikolettviola19@gmail.com>
2022-02-22 09:38:23 +0100

    első kommit

---
+++ ikozagon.java
@@ -0,0 +1,13 @@
+/*
+* File: ikozagon.java
+* Author: Szilágyi Nikolett Viola
+* Copyright: 2022, Szilágyi Nikolett Viola
+* Group: Szoft_I_N
+* Date: 2022-02-16
+* Github: https://github.com/nikolettszilagyii/
+* Licenc: GNU GPL
+*/
+
+import java.until.Scanner
+
+class  
